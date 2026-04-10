1697898136fb88c7967b9626282586ca8c1a6740
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 20:04:57 +0530

    added backend url placeholder

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,22 @@
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
+// This checks for the environment variable, or falls back to localhost
+const SOCKET_URL = BASE_URL || "http://localhost:3000";
+
 export const createSocketConnection = () => {
-    // return io(BASE_URL)
-    if(location.hostname === "localhost"){
-        return io(BASE_URL);
-    }
-    else{
-        return io("/", {path: "/api/socket.io"});
-    }
-}
+    return io(SOCKET_URL);
+};
+
+// import io from "socket.io-client";
+// import { BASE_URL } from "./constants";
+
+// export const createSocketConnection = () => {
+//     // return io(BASE_URL)
+//     if(location.hostname === "localhost"){
+//         return io(BASE_URL);
+//     }
+//     else{
+//         return io("/", {path: "/api/socket.io"});
+//     }
+// }
