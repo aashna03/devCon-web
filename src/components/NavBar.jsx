d11dceecc57e55905bece77d9c672840eee76f85
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 23:21:51 +0530

    added vercel.json

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -73,7 +73,7 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
               <li>
                 <Link to="/edit" className="justify-between">
-                  Profile Edit
+                   Edit Profile
                 </Link>
               </li>
               <li>
