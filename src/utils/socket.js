1baf2c8c8dfc90742a607cfbe29180b081bbf2d8
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 22:16:13 +0530

    socket correction

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,13 +3,14 @@ import { BASE_URL } from "./constants";
 
 // const backendUrl = `${BASE_URL}`;
 export const createSocketConnection = () => {
-    return io(BASE_URL, {path: "/socket.io"}, {
+    return io(BASE_URL, {
+        path: "/socket.io", 
         withCredentials: true,
         reconnection: true,
         reconnectionDelay: 1000,
         reconnectionDelayMax: 5000,
         reconnectionAttempts: 5,
-        transports: ["polling", "websocket"],
+        transports: ["websocket", "polling"], 
     });
 };
 
