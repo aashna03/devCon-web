056c2ee7c42d415dcc4b8f964c6cf2fdcc62e1a3
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 21:44:30 +0530

    socket url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,6 @@
 
 const backendUrl =
 	import.meta.env.VITE_BACKEND_URL ||
-	import.meta.env.BACKEND_URL ||
 	"http://localhost:3000";
 
-export const BASE_URL = backendUrl.replace(/\/$/, "");
+export const BASE_URL = backendUrl;
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 // This checks for the environment variable, or falls back to localhost
-const SOCKET_URL = BASE_URL || "http://localhost:3000";
+const SOCKET_URL = `${BASE_URL}/socket.io` || "http://localhost:3000";
 
 export const createSocketConnection = () => {
     return io(SOCKET_URL);
