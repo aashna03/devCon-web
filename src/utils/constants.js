d592dd26ed9035508ce95fe7e3849876e41a0bf3
Aashna Agarwal <72308752+aashna03@users.noreply.github.com>
2026-04-10 20:20:52 +0530

    corrected url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,9 @@
 // export const BASE_URL = 
 //     location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
-export const BASE_URL = process.env.BACKEND_URL || "http://localhost:3000";
+const backendUrl =
+	import.meta.env.VITE_BACKEND_URL ||
+	import.meta.env.BACKEND_URL ||
+	"http://localhost:3000";
+
+export const BASE_URL = backendUrl.replace(/\/$/, "");
